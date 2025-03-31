2b4fc5b647fa77042ce28fae3c9a34c2081ff58e
AbelGetu <abelgetu27@gmail.com>
2025-03-31 07:03:00 +0300

    adding explore header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,24 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import React, { useState } from 'react'
+import { Link, Stack } from 'expo-router'
+import ExploreHeader from '@/components/ExploreHeader'
+import Listings from '@/components/Listings'
 
 const Page = () => {
+  const [category, setCategory] = useState('Tiny homes');
+  const onDataChanged = (category: string) => {
+    setCategory(category);
+  }
+
   return (
-    <View>
-      <Link href={'/(modals)/login'}>Login</Link>
-      <Link href={'/(modals)/booking'}>Booking</Link>
-      <Link href={{ 
-        pathname: '/listing/[id]',
-        params: {id: '1234'}
-       }}>Listing Details</Link>
+    <View style={{ flex: 1, marginTop: 160 }}>
+      <Stack.Screen 
+        options={{ 
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />
+         }}
+      />
+
+      <Listings listings={[]} category={category} />
     </View>
   )
 }
